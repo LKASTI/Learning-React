f07b704a41c042f262c2b9c202334ddd6f17b3f3
Landin Kasti <lkastiman@gmail.com>
2022-11-17 14:41:48 -0600

    commiting generateExcuse react program

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
+import axios from 'axios'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [excuse, setExcuse] = useState('')
+
+  const fetchExcuse = (excuseType) => {
+    axios.get(`https://excuser.herokuapp.com/v1/excuse/${excuseType}/`).then((res) =>{
+      setExcuse(res.data[0].excuse)
+      // console.log(res.data[0])
+    })
+  }
+  
+  return(
+    <div className ="App">
+      <h1>Generate Excuse</h1>
+      <button onClick={() => {fetchExcuse('party')}}> Party</button>
+      <button onClick={() => {fetchExcuse('family')}}> Family</button>
+      <button onClick={() => {fetchExcuse('office')}}> Office</button>
+      
+      <p>{excuse}</p>
     </div>
-  );
+  )
 }
 
 export default App;
